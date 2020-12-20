1cfc104e915c14a33587a4b40ec52d1e81b1de24
Heiko Radde <heiko-rudow@live.de>
2020-12-20 19:23:54 +0100

    Added scripts for windows and linux export

---
+++ export_linux.sh
@@ -0,0 +1,3 @@
+/usr/lib/jvm/jdk-14.0.2/bin/jpackage --name "THW-MATP" --input out/artifacts/THW_MATP_jar --main-jar THW-MATP.jar --app-version 1.0.0 --linux-app-release 1.0.0 --copyright "2020 Heiko Radde" --description "THW-MATP Programm für die Materialprüfung im THW" --linux-deb-maintainer "heiko-rudow@live.de" --linux-shortcut --linux-rpm-license-type "MIT"
+
+# Tutorial: https://openjdk.java.net/jeps/343#Platform-specific-details
