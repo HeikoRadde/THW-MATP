de393ae64b6d046ef43388c424462b08e533e7c6
Heiko Radde <heiko-rudow@live.de>
2021-04-13 20:32:29 +0200

    moved windows-export-info to .txt

--- export_windows.ps1
+++
@@ -1,1 +0,0 @@
-﻿& 'C:\Program Files\Java\jdk-14.0.2\bin\jpackage.exe' --name "THW-MATP" --input out/artifacts/THW_MATP_jar --main-jar THW-MATP.jar --app-version 1.0.0 --copyright "2020 Heiko Radde" --description "THW-MATP Programm für die Materialprüfung im THW" --win-dir-chooser --win-menu --win-shortcut
